e3cff9321813cac1694ecc3e07a2f2172f882bba
NikolaYordanov94 <106193855+NikolaYordanov94@users.noreply.github.com>
2022-09-18 10:13:01 +0300

    Add files via upload

---
+++ WordFilter_04.java
@@ -0,0 +1,32 @@
+package AssociativeArrays;
+
+import java.util.*;
+
+public class WordFilter_04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] wordsArr = Arrays.stream(scanner.nextLine().split(" "))
+                .filter(e -> e.length() % 2 == 0)
+                .toArray(String[]::new);
+
+        System.out.println(String.join(System.lineSeparator(), wordsArr));
+
+
+//        String[] wordsArr = scanner.nextLine().split(" ");
+//
+//        List<String> resultList = new ArrayList<>();
+//        for (int i = 0; i < wordsArr.length; i++) {
+//            int len = wordsArr[i].length();
+//
+//            if (len % 2 == 0) {
+//                resultList.add(wordsArr[i]);
+//            }
+//        }
+//
+//        for (String s : resultList) {
+//            System.out.println(s);
+//        }
+
+    }
+}

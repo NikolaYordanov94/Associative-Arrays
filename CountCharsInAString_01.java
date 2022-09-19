492c605406d5eddcc0822f9fbdefbf80fdd82bf1
NikolaYordanov94 <106193855+NikolaYordanov94@users.noreply.github.com>
2022-09-19 22:11:12 +0300

    Add files via upload

---
+++ CountCharsInAString_01.java
@@ -0,0 +1,36 @@
+package AssociativeArraysExercise;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CountCharsInAString_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+
+        Map<Character, Integer> symbolsCount = new LinkedHashMap<>();
+
+        for (char symbol : text.toCharArray()) {
+
+            if (symbol == ' ') {
+                continue;
+            }
+
+            if (!symbolsCount.containsKey(symbol)) {
+
+                symbolsCount.put(symbol, 1);
+            } else {
+                int currentCount = symbolsCount.get(symbol);
+                symbolsCount.put(symbol, currentCount + 1);
+            }
+        }
+
+        // речник: съмвол -> броя на срещанията
+
+        symbolsCount.entrySet().
+                forEach(entry -> System.out.println(entry.getKey() + " -> " + entry.getValue()));
+
+    }
+}
